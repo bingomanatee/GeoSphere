5c89b43597ecdd6feeea4cf36461106c6ccf5335
David Edelhart <dave@wonderlandlabs.com>
2013-08-06 07:54:25 -0700

    generated compressed data import/export for albedo and cloud cover.

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = {
     Planet: require('./lib/Planet.js'),
     util: require('./lib/util'),
     climate: require('./lib/climate'),
-    CLIMATE_ROOT: path.resolve(__dirname,  '../climate_data')
+    CLIMATE_ROOT: path.resolve(__dirname,  '../climate_data'),
+    CLIMATE_BINARY: path.resolve(__dirname, 'climate_binary_data')
 };
---
+++ write_binary_data.js
@@ -0,0 +1,40 @@
+var _ = require('underscore');
+var path = require('path');
+var fs = require('fs');
+var Gate = require('gate');
+
+var MIN_DEPTH = 0;
+var MAX_DEPTH = 4;
+var WIDTH = 720;
+var HEIGHT = 360;
+
+var mode = 'clouds';
+
+if (process.argv.length > 2) {
+    mode = process.argv[2];
+    if (process.argv.length > 3) {
+
+        MIN_DEPTH = parseInt(process.argv[3]);
+        MAX_DEPTH = parseInt(process.argv[4]);
+
+        if (process.argv.length > 5) {
+            WIDTH = parseInt(process.argv[5]);
+            HEIGHT = parseInt(process.argv[6]);
+        }
+    }
+}
+
+console.log('WRITING BINARY DATA for %s: depths %s ... %s, %s px X %s px images',  mode, MIN_DEPTH, MAX_DEPTH, WIDTH, HEIGHT)
+
+switch (mode) {
+    case 'clouds':
+        require('./lib/binary/clouds')(MIN_DEPTH, MAX_DEPTH, WIDTH, HEIGHT);
+        break;
+
+    case 'albedo':
+        require('./lib/binary/albedo')(MIN_DEPTH, MAX_DEPTH, WIDTH, HEIGHT);
+        break;
+
+    default:
+        throw new Error('cannot find mode ' + mode);
+}
